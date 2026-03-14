34e7eece0ee8f57e4e49b7fbf361a49268593619
Eyoxee <bergergaby005@gmail.com>
2026-03-14 23:07:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const selectorList = document.getElementById("selector-list");
 const selectorClose = document.getElementById("selector-close");
 const GITHUB_USER = "Eyoxee";
 const GITHUB_REPO = "Spraywall-Arkose-Bordeaux";
-const GITHUB_TOKEN = "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";
+const GITHUB_TOKEN = "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";
 
 let holds = [
   {
