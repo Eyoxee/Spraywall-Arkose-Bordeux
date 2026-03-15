5bc1d5056a0348ae9d15754bdcdfc7a0a2556bd9
Eyoxee <bergergaby005@gmail.com>
2026-03-15 12:14:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1195,9 +1195,11 @@ async function saveBlocsToGitHub(blocs) {
     const putRes = await fetch(url, {
         method: "PUT",
         headers: {
-            "Authorization": `token ${GITHUB_TOKEN}`,
+            "Authorization": `Bearer ${GITHUB_TOKEN}`,
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
             "Content-Type": "application/json"
-        },
+          },
         body: JSON.stringify({
             message: "Update blocs.json",
             content: newContent,
